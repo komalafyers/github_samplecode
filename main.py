d5f75407dc879edd5f6a911fcad2dcd73246934a
komalafyers <85447305+komalafyers@users.noreply.github.com>
2021-06-11 10:50:12 +0530

    to test diff

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ def NumberofHouse(r, unit, arr):
         else:
             return 0:
 print(NumberofHouse(7,2,[2,8,3,5,4,1,2]))
-
-
-
-
-
-
-
+print(NumberofHouse(8,9,[3,6,5,8,4,64,10]))
+            
 
 
 n=input("enter the name")
 print(n)
+
+ef NumberofHouse(r, unit, arr):
+    rats=r*unit
+    count=0
+    count=1
             
         
     
